6721a0c419d7c4e80ee4b8b55d78576cce885e90
lilydove <wuyiwangyi@163.com>
2017-05-29 16:35:09 +0800

    2017-05-29 修正了Tabs显示不出来的问题

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.administrator.myapplication;
 
 import android.content.Intent;
+//import android.app.ActionBar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,6 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get the Activity of ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        //Set the operating model of ActionBar
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        //Get rid of the head of Activity
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle("Tab 测试");
+        //create Tab
+        ActionBar.Tab tab1 = actionBar.newTab().setText("No1");
+        ActionBar.Tab tab2 = actionBar.newTab().setText("No2");
+        ActionBar.Tab tab3 = actionBar.newTab().setText("No3");
+        //Add Listener for every Tab
+        MyTabListener tab1Listener = new MyTabListener();
+        tab1.setTabListener(tab1Listener);
+        MyTabListener tab2Listener = new MyTabListener();
+        tab2.setTabListener(tab2Listener);
+        MyTabListener tab3Listener = new MyTabListener();
+        tab3.setTabListener(tab3Listener);
+        //Add Tab into ActionBar
+        actionBar.addTab(tab1);
+        actionBar.addTab(tab2);
+        actionBar.addTab(tab3);
+
     }
 
     /** called when the user clicks the Send button*/
---
+++ app/src/main/java/com/example/administrator/myapplication/MyTabListener.java
@@ -0,0 +1,27 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.TabListener;
+import android.app.FragmentTransaction;
+
+/**
+ * Created by Administrator on 2017/5/29.
+ */
+
+public class MyTabListener implements TabListener {
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+
+    }
+}
